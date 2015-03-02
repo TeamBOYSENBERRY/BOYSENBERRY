b71a6818d738cc671f49176ecad6e2e89ef8da0d
brslv <brslvg@gmail.com>
2015-03-02 22:16:10 +0200

    Added team.php page. Styled it a bit. Please, fill in your contact details, team! :)

---
+++ team.php
@@ -0,0 +1,98 @@
+<?php include 'incl/header.php'; ?>
+
+	<main class="main-content container-s">
+		<section>
+			<article class="member">
+				<div class="section group">
+					<div class="col span_9_of_12">
+						<h3 class="member-name">Борислав Григоров</h3>
+					</div>
+
+					<div class="col span_3_of_12 member-info">
+						<a href="">facebook</a>
+						<a href="">twitter</a>
+					</div>
+				</div>
+			</article> 
+
+			<article class="member">
+				<div class="section group">
+					<div class="col span_9_of_12">
+						<h3 class="member-name">Тихомир Тодоров</h3>
+					</div>
+
+					<div class="col span_3_of_12 member-info">
+						<a href="">facebook</a>
+						<a href="">twitter</a>
+					</div>
+				</div>
+			</article> 
+
+			<article class="member">
+				<div class="section group">
+					<div class="col span_9_of_12">
+						<h3 class="member-name">Ивелин Митев</h3>
+					</div>
+
+					<div class="col span_3_of_12 member-info">
+						<a href="">facebook</a>
+						<a href="">twitter</a>
+					</div>
+				</div>
+			</article> 
+
+			<article class="member">
+				<div class="section group">
+					<div class="col span_9_of_12">
+						<h3 class="member-name">Кирил Янков</h3>
+					</div>
+
+					<div class="col span_3_of_12 member-info">
+						<a href="">facebook</a>
+						<a href="">twitter</a>
+					</div>
+				</div>
+			</article> 
+
+			<article class="member">
+				<div class="section group">
+					<div class="col span_9_of_12">
+						<h3 class="member-name">Андриян Масларов</h3>
+					</div>
+
+					<div class="col span_3_of_12 member-info">
+						<a href="">facebook</a>
+						<a href="">twitter</a>
+					</div>
+				</div>
+			</article> 
+
+			<article class="member">
+				<div class="section group">
+					<div class="col span_9_of_12">
+						<h3 class="member-name">Стоян Петров</h3>
+					</div>
+
+					<div class="col span_3_of_12 member-info">
+						<a href="">facebook</a>
+						<a href="">twitter</a>
+					</div>
+				</div>
+			</article> 
+
+			<article class="member">
+				<div class="section group">
+					<div class="col span_9_of_12">
+						<h3 class="member-name">Милена Иванова</h3>
+					</div>
+
+					<div class="col span_3_of_12 member-info">
+						<a href="">facebook</a>
+						<a href="">twitter</a>
+					</div>
+				</div>
+			</article> 
+		</section>
+	</main>
+
+<?php include 'incl/footer.php'; ?>
