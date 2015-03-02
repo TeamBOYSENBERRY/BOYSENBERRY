388511f0fd6de7e1b076da7fb5aa1c1079850135
brslv <brslvg@gmail.com>
2015-03-02 21:41:42 +0200

    Added fails.php

---
+++ fails.php
@@ -0,0 +1,28 @@
+<?php include 'incl/header.php'; ?>
+
+	<main class="main-content container-s">
+		<section class="section group">
+			<article class="col span_12_of_12">
+				<header>
+					<h2 class="center-text">Когато не знаеш какво да кажеш, по-добре мълчи.</h2>
+				</header>
+
+				<div class="article-content">
+									
+					<hr />
+
+					<p>
+						По-долу ще намериш достатъчно материал, за да придобиеш представа какво НЕ трябва да правиш в SoftUni.
+					</p>
+
+					<hr />
+
+					<iframe src="https://www.youtube.com/embed/1LBfr2UVm-A" frameborder="0" allowfullscreen></iframe>
+
+					<iframe src="https://www.youtube.com/embed/91w_pC0IN80" frameborder="0" allowfullscreen></iframe>
+				</div>
+			</article>
+		</section>
+	</main>
+
+<?php include 'incl/footer.php'; ?>
