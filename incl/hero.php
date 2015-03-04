7d8aeef3d588fb54ffa1e99150ac4b6d6b720971
brslv <brslvg@gmail.com>
2015-03-04 10:15:40 +0200

    Fixed styles, some were completely broken (logo positioning, info-box-images and so on...).; fixed info-box-titles and icons displaing; fixed team social icon buttons; added call to action button, leading to the openers page

--- incl/hero.php
+++ incl/hero.php
@@ -6,4 +6,8 @@
 		<div class="description">
 			<h4 class="font-light">СофтУни opener-и за 'дон-жуан-девове'.</h4>
 		</div> <!-- End of .description -->
+
+		<div class="call-to-action">
+			<a href="pick-ups-soft.php" class="btn-accent">Към опънърите &rarr;</a>
+		</div>
 	</aside>
--- team.php
+++ team.php
@@ -23,9 +23,8 @@
 					</div>
 
 					<div class="col span_3_of_12 member-info">
-						<a href="http://facebook.com/grigorov.b" title="Facebook"><img src="img/icons/social_facebook_circle.png"></a>
-						<a href="https://twitter.com/brslv" title="Twitter"><img src="img/icons/social_twitter_circle.png"></a>
-						<a href="https://softuni.bg/users/profile/show/brslv" title="SoftUni"><img src="img/icons/icon_id-2_alt.png"></a>
+						<a href="https://www.facebook.com/task.force.775" title="Facebook"><img src="img/icons/social_facebook_circle.png"></a>
+						<a href="https://softuni.bg/users/profile/show/tisho_todorov" title="SoftUni"><img src="img/icons/icon_id-2_alt.png"></a>
 					</div>
 				</div>
 			</article> 
@@ -37,9 +36,7 @@
 					</div>
 
 					<div class="col span_3_of_12 member-info">
-						<a href="http://facebook.com/grigorov.b" title="Facebook"><img src="img/icons/social_facebook_circle.png"></a>
-						<a href="https://twitter.com/brslv" title="Twitter"><img src="img/icons/social_twitter_circle.png"></a>
-						<a href="https://softuni.bg/users/profile/show/brslv" title="SoftUni"><img src="img/icons/icon_id-2_alt.png"></a>
+						<a href="https://softuni.bg/users/profile/show/re5pect" title="SoftUni"><img src="img/icons/icon_id-2_alt.png"></a>
 					</div>
 				</div>
 			</article> 
@@ -51,9 +48,7 @@
 					</div>
 
 					<div class="col span_3_of_12 member-info">
-						<a href="http://facebook.com/grigorov.b" title="Facebook"><img src="img/icons/social_facebook_circle.png"></a>
-						<a href="https://twitter.com/brslv" title="Twitter"><img src="img/icons/social_twitter_circle.png"></a>
-						<a href="https://softuni.bg/users/profile/show/brslv" title="SoftUni"><img src="img/icons/icon_id-2_alt.png"></a>
+						<a href="https://softuni.bg/users/profile/show/nok32" title="SoftUni"><img src="img/icons/icon_id-2_alt.png"></a>
 					</div>
 				</div>
 			</article> 
@@ -65,9 +60,8 @@
 					</div>
 
 					<div class="col span_3_of_12 member-info">
-						<a href="http://facebook.com/grigorov.b" title="Facebook"><img src="img/icons/social_facebook_circle.png"></a>
-						<a href="https://twitter.com/brslv" title="Twitter"><img src="img/icons/social_twitter_circle.png"></a>
-						<a href="https://softuni.bg/users/profile/show/brslv" title="SoftUni"><img src="img/icons/icon_id-2_alt.png"></a>
+						<a href="https://www.facebook.com/anm7bg" title="Facebook"><img src="img/icons/social_facebook_circle.png"></a>
+						<a href="https://softuni.bg/users/profile/show/amaslarov" title="SoftUni"><img src="img/icons/icon_id-2_alt.png"></a>
 					</div>
 				</div>
 			</article> 
@@ -79,9 +73,8 @@
 					</div>
 
 					<div class="col span_3_of_12 member-info">
-						<a href="http://facebook.com/grigorov.b" title="Facebook"><img src="img/icons/social_facebook_circle.png"></a>
-						<a href="https://twitter.com/brslv" title="Twitter"><img src="img/icons/social_twitter_circle.png"></a>
-						<a href="https://softuni.bg/users/profile/show/brslv" title="SoftUni"><img src="img/icons/icon_id-2_alt.png"></a>
+						<a href="https://www.facebook.com/stoyan.petrov.92" title="Facebook"><img src="img/icons/social_facebook_circle.png"></a>
+						<a href="https://softuni.bg/users/profile/show/heat661" title="SoftUni"><img src="img/icons/icon_id-2_alt.png"></a>
 					</div>
 				</div>
 			</article> 
@@ -93,9 +86,7 @@
 					</div>
 
 					<div class="col span_3_of_12 member-info">
-						<a href="http://facebook.com/grigorov.b" title="Facebook"><img src="img/icons/social_facebook_circle.png"></a>
-						<a href="https://twitter.com/brslv" title="Twitter"><img src="img/icons/social_twitter_circle.png"></a>
-						<a href="https://softuni.bg/users/profile/show/brslv" title="SoftUni"><img src="img/icons/icon_id-2_alt.png"></a>
+						<a href="https://softuni.bg/users/profile/show/m.zh.ivanova" title="SoftUni"><img src="img/icons/icon_id-2_alt.png"></a>
 					</div>
 				</div>
 			</article> 
